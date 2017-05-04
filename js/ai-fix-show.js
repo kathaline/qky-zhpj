def67ff0b87c663869be441de29ece4b731662fc
kathaline <yhc401@163.com>
2017-05-04 15:02:18 +0800

    调整

--- js/ai-fix-show.js
+++ js/ai-fix-show.js
@@ -9,3 +9,12 @@ $(function() {
         $(elm).css('top',((p) > startPos) ? '50px' : ''); 
     }); 
 }); 
+$(function() { 
+    var elm = $('.po_fixed'); 
+    var startPos = $(elm).offset().top; 
+    $.event.add(window, "scroll", function() { 
+        var p = $(window).scrollTop(); 
+        $(elm).css('position',((p) > startPos) ? 'fixed' : 'static'); 
+        $(elm).css('top',((p) > startPos) ? '50px' : ''); 
+    }); 
+}); 

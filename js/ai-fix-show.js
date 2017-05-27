8cb739b6e646d3f9ff6a664b64886200a96bc4a0
kathaline <yhc401@163.com>
2017-05-27 14:25:50 +0800

    配色样式调整

--- js/ai-fix-show.js
+++ js/ai-fix-show.js
@@ -7,14 +7,7 @@ $(function() {
         var p = $(window).scrollTop(); 
         $(elm).css('position',((p) > startPos) ? 'fixed' : 'static'); 
         $(elm).css('top',((p) > startPos) ? '50px' : ''); 
+		$('#concon').css('margin-top',((p) > startPos) ? '287px' : '0px')
     }); 
 }); 
-$(function() { 
-    var elm = $('.po_fixed'); 
-    var startPos = $(elm).offset().top; 
-    $.event.add(window, "scroll", function() { 
-        var p = $(window).scrollTop(); 
-        $(elm).css('position',((p) > startPos) ? 'fixed' : 'static'); 
-        $(elm).css('top',((p) > startPos) ? '50px' : ''); 
-    }); 
-}); 
+

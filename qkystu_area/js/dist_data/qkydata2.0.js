34c54287c22a7b6198fdd980d6e97285a75541fc
kathaline <yhc401@163.com>
2017-06-20 14:50:20 +0800

    谢谢

--- qkystu_area/js/dist_data/qkydata2.0.js
+++ qkystu_area/js/dist_data/qkydata2.0.js
@@ -15,7 +15,7 @@ define({
 		},//个性定制专属的应用名颜色和主导航选中颜色
 		distclass:["navbar", "navbar-fixed-top", "headernav"],//默认一定要用的class
 		navli_j:[
-			{name:"评价督导工作",attrcont:"href='index.html'",isactive:false},
+			{name:"评价督导工作",attrcont:"href='index.html'",isactive:true},
 			{name:"评价体系管理",attrcont:"href='index-sysm.html'",isactive:false},
 			{name:"大数据分析",attrcont:"href=''",isactive:false},
 			{name:"成长档案袋",attrcont:"href=''",isactive:false},

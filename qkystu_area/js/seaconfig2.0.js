8991c932006d7d7aac0a34fd79882a93ac780c32
kathaline <yhc401@163.com>
2017-06-20 16:11:32 +0800

    从v不v

--- qkystu_area/js/seaconfig2.0.js
+++ qkystu_area/js/seaconfig2.0.js
@@ -49,10 +49,10 @@ seajs.config({
 
 var opt={
 	navli_j:[
-			{name:"评价督导工作",attrcont:"href='index.html'",isactive:false},
-			{name:"评价体系管理",attrcont:"href='index-sysm.html'",isactive:false},
-			{name:"大数据分析",attrcont:"href=''",isactive:false},
-			{name:"成长档案袋",attrcont:"href=''",isactive:false},
+			{name:"评价信息",attrcont:"href='index.html'",isactive:false},
+			{name:"数据监控",attrcont:"href='index-dm.html'",isactive:false},
+			{name:"数据分析",attrcont:"href='index-da.html'",isactive:false},
+			{name:"成长档案袋",attrcont:"href='index-grow.html'",isactive:false},
 	]
 }
 

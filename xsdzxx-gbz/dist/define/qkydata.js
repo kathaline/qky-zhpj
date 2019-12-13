56d6f0ba970987eb47ebbea2cda1ee39ab6ef78c
kathaline <yhc401@163.com>
2019-12-13 16:22:23 +0800

    学生电子信息

---
+++ xsdzxx-gbz/dist/define/qkydata.js
@@ -0,0 +1,23 @@
+
+/****
+默认数据存放
+****/ 
+
+define({
+	navdata:{/*导航数据*/
+		"logosrc":"images/nav_logo/logo.png",
+		"pjname":"学生信息管理",
+		"navli":[["基础数据管理","href='index_basic-now.html'"],["学生基本信息","href='index.html'"],["学生扩展信息","href='index_gl.html'"],["学生信息统计","href='index_tj.html'"]],
+		"navli_active":0,
+		"moreli":{
+			"校园办公":["xiaoyuanbangong",[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]]],
+			"校园支付":["xiaoyuanzhifu",[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]]],
+			"智慧教学":["zhihuijiaoxue",[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]]]
+		},
+		"morebtn":false,
+		"common":[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]],
+		"lately":[["个人办公","href='#'"],["行政办公","href='#'"],["流程审批","href='#'"]],
+		"tea_info":{"name":"张晓明","photo":""},
+		"otherli":[["个人空间","href='#'"],["退出","id='toOut'"]]
+	}
+});

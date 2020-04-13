0d91938b2ae45cc3939cdf4c26f9c6c6e7ee78db
katahline <yhc401@163.com>
2020-04-13 15:05:59 +0800

    超过两行管理员隐藏，按钮展开收起交互

--- Daily-evaluation/js/seth.js
+++ Daily-evaluation/js/seth.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
  
-  var len=8;//要显示的数量
+  var len=8;
   var arr=$(".set-height .col-sm-3:not(:hidden)");
     if(arr.length<=len){
-        $(".seth_btn").hide();
+      $(".seth_div").hide();
         }
     if(arr.length>len){
     $('.set-height .col-sm-3:gt('+(len-1)+')').hide();
@@ -12,11 +12,13 @@ $(document).ready(function(){
         var arr=$(".set-height .col-sm-3:not(:hidden)");
       if(arr.length>len){
          $('.set-height .col-sm-3:gt('+(len-1)+')').hide();
-         $(".seth_btn").val("展开更多管理员");
+         $(".seth_btn").html("请展开更多管理员 ▼");
+         $(".seth_div").css({"position": "absolute","right": "0","top":"102px","display":"block",});
       }
        else{
          $('.set-height .col-sm-3:gt('+(len-1)+')').show();
-         $(".seth_btn").val("收起更多管理员");
+         $(".seth_btn").html("请收起更多管理员 ▲");
+         $(".seth_div").css({"position": "relative","left":"auto" ,"top":"auto","right": "auto","float": "left",});
        }
    });
  });

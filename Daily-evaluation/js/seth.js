02e034c0e724a85abd54e5bedb789168311619d6
katahline <yhc401@163.com>
2020-04-13 15:08:48 +0800

    00

--- Daily-evaluation/js/seth.js
+++ Daily-evaluation/js/seth.js
@@ -12,12 +12,12 @@ $(document).ready(function(){
         var arr=$(".set-height .col-sm-3:not(:hidden)");
       if(arr.length>len){
          $('.set-height .col-sm-3:gt('+(len-1)+')').hide();
-         $(".seth_btn").html("请展开更多管理员 ▼");
+         $(".seth_btn").html("请展开更多管理员  ▼");
          $(".seth_div").css({"position": "absolute","right": "0","top":"102px","display":"block",});
       }
        else{
          $('.set-height .col-sm-3:gt('+(len-1)+')').show();
-         $(".seth_btn").html("请收起更多管理员 ▲");
+         $(".seth_btn").html("请收起更多管理员  ▲");
          $(".seth_div").css({"position": "relative","left":"auto" ,"top":"auto","right": "auto","float": "left",});
        }
    });

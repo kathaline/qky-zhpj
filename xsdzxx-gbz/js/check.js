fcdaae72e51a4be4c10c9761a0ed6bde4b5114b1
katahline <yhc401@163.com>
2020-04-17 17:20:33 +0800

    v2.2

--- xsdzxx-gbz/js/check.js
+++ xsdzxx-gbz/js/check.js
@@ -8,3 +8,51 @@ define(function(require) {
 
 })
 
+$('input[type="checkbox"]').change(function(e) {
+
+    var checked = $(this).prop("checked"),
+        container = $(this).parent(),
+        siblings = container.siblings();
+
+    container.find('input[type="checkbox"]').prop({
+        indeterminate: false,
+        checked: checked
+    });
+
+    function checkSiblings(el) {
+
+        var parent = el.parent().parent(),
+            all = true;
+        el.siblings().each(function() {
+            var returnValue = all = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
+            return returnValue;
+        });
+
+        if (all && checked) {
+
+            parent.children('input[type="checkbox"]').prop({
+                indeterminate: false,
+                checked: checked
+            });
+
+            checkSiblings(parent);
+
+        } else if (all && !checked) {
+
+            parent.children('input[type="checkbox"]').prop("checked", checked);
+            parent.children('input[type="checkbox"]').prop("indeterminate", (parent.find('input[type="checkbox"]:checked').length > 0));
+            checkSiblings(parent);
+
+        } else {
+
+            el.parents("li").children('input[type="checkbox"]').prop({
+                indeterminate: true,
+                checked: false
+            });
+
+        }
+
+    }
+
+    checkSiblings(container);
+});
